66aa8528ade916531fd8bc4d353d9519963e264c
send2r <send2r@6e06423a-ca39-0410-a06b-e7f3f123f321>
2007-09-11 21:13:21 +0000

    Implementing basic data forms

---
+++ trunk/src/org/ranjith/GoBackAction.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package org.ranjith;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * @author XR1CTSO
+ *
+ */
+public class GoBackAction implements ActionListener {
+    private TestFrame testFrame;
+    public GoBackAction(TestFrame testFrame) {
+        this.testFrame = testFrame;
+    }
+
+    /* (non-Javadoc)
+     * @see java.awt.event.ActionListener#actionPerformed(java.awt.event.ActionEvent)
+     */
+    @Override
+    public void actionPerformed(ActionEvent arg0) {
+        testFrame.restoreUI();
+    }
+
+}
