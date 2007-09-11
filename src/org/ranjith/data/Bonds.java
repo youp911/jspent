1e0d751462cea8243a64691e3261961b553dca2a
send2r <send2r@6e06423a-ca39-0410-a06b-e7f3f123f321>
2007-09-11 16:44:20 +0000

    Interim commit

---
+++ src/org/ranjith/data/Bonds.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package org.ranjith.data;
+
+/**
+ * @author XR1CTSO
+ *
+ */
+public class Bonds extends Saving {
+    private String financialInstitution;
+
+    /**
+     * @return the financialInstitution
+     */
+    public String getFinancialInstitution() {
+        return financialInstitution;
+    }
+
+    /**
+     * @param financialInstitution the financialInstitution to set
+     */
+    public void setFinancialInstitution(String financialInstitution) {
+        this.financialInstitution = financialInstitution;
+    }
+    
+ 
+}
