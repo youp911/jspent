11d50342dce4fece9841bbd3b80a0d42548ae577
send2r <send2r@6e06423a-ca39-0410-a06b-e7f3f123f321>
2007-09-14 20:52:36 +0000

    Initial plugin manager - needs to re-think

---
+++ src/org/ranjith/util/PluginManager.java
@@ -0,0 +1,48 @@
+/**
+ * 
+ */
+package org.ranjith.util;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.digester.Digester;
+import org.apache.commons.digester.xmlrules.DigesterLoader;
+import org.ranjith.TestFrame;
+import org.ranjith.plugin.GenericPlugin;
+import org.xml.sax.SAXException;
+
+/**
+ * @author XR1CTSO
+ *
+ */
+public class PluginManager {
+    private static List plugins;
+    public PluginManager() {
+    }
+    public static List savingsPluginList(String file) {
+        try{
+            if(plugins == null)
+                loadSavingsPlugins();
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
+        return plugins;
+    }
+    
+    private static void loadSavingsPlugins() throws IOException, SAXException {
+       plugins = new ArrayList();
+       Digester digester = DigesterLoader.createDigester(PluginManager.class.getResource("../../../plugins-config/digester-rules.xml"));
+       PluginManager pm = (PluginManager) digester.parse(PluginManager.class.getResource("../../../plugins-config/savings-plugins.xml").getFile());
+    }
+    public void addPlugin(GenericPlugin p) {
+        plugins.add(p);
+    }
+    
+    public static void main(String[] args) {
+        List l = PluginManager.savingsPluginList("");
+        System.out.println(l);
+    }
+}
